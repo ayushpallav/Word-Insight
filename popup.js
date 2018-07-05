a3fedd6998f02f04bc4605d199aa52ea2be5b967
shubhamdhonde <38417155+shubhamdhonde@users.noreply.github.com>
2018-07-05 18:37:42 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,1 @@
-var port = chrome.extension.connect({
-    name: "Sample Communication"
-});
-port.postMessage("Hi BackGround");
-port.onMessage.addListener(function(msg) {
-    var mm = "<li>" + msg + "</li>" + document.getElementById('x').innerHTML;
-    document.getElementById('x').innerHTML =  mm;
-});
+
